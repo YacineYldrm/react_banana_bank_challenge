be9d4d3a0b288900ebc6d5f3e4f665a8bd9570b4
YacineYldrm <yacine@mailbox.org>
2023-11-29 11:34:46 +0100

    optimized input-field behavior for user input

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,37 +5,39 @@ const Calculator = () => {
 
     const [value, setValue] = useState("");
     const [debited, setDebited] = useState("");
+    const [paidIn, setPaidIn] = useState("");
     const [bankBalance, setBankBalance] = useState("");
 
     console.log(debited);
 
     const deposit = () =>
     {
-        if(Number(value) <= 10000 && Number(value) > 0 && (Number(bankBalance) + Number(value)) <=100000)
+        if(Number(value) <= 5000 && Number(value) > 0 && (Number(bankBalance) + Number(paidIn)) <=10000)
         {
             setBankBalance(Number(bankBalance) + Number(value));
+            setPaidIn(Number(paidIn) + Number(value));
             setValue("");
         }
-        else if(Number(value) > 10000)
+        else if(Number(value) > 5000)
         {
-            window.alert("Maximalbetrag pro Einzahlung: 10.000€");
+            window.alert("Maximalbetrag pro Einzahlung: 5.000€");
             setValue("");
         }
-        else if(Number(value) <= 0)
+        else if(Number(value) + Number(paidIn) > 10000)
         {
-            window.alert("Bitte einen höhren Betrag als 0.00€ eingeben");
+            window.alert("Das Tageslimit für Einzahlungen beträgt 10.000€. Bitte wenden Sie sich an Ihre*n Berater*in.");
             setValue("");
         }
         else
         {
-            window.alert("Das Tageslimit für Einzahlungen beträgt 100.000€. Bitte wenden Sie sich an Ihre*n Berater*in.");
+            window.alert("Bitte einen höhren Betrag als 0.00€ eingeben");
             setValue("");
         }
     }  
 
     const debit = () =>
     {
-        if(Number(value) <= Number(bankBalance) && Number(value) <= 5000 && (Number(debited) + Number(value)) <= 5000)
+        if(Number(value) <= Number(bankBalance) && Number(value) <= 5000 && (Number(debited) + Number(value)) <= 5000 && Number(value) > 0)
         {
             setBankBalance(bankBalance - value);
             setDebited(Number(debited) + Number(value));
@@ -50,6 +52,22 @@ const Calculator = () => {
         {
             window.alert('Das Tageslimit für Abbuchungen beträgt 5.000€. Bitte wenden Sie sich an Ihre*n Berater*in.')
         }
+        else
+        {
+            window.alert("Bitte einen höhren Betrag als 0.00€ eingeben");
+            setValue("");
+        }
+    }
+
+    const getUserInput = (input) =>
+    {
+        if (Number(input.target.value) >= 1) {
+            setValue(Number(input.target.value));
+        }
+        else
+        {
+            setValue("");
+        }
     }
 
     return ( 
@@ -61,7 +79,7 @@ const Calculator = () => {
             name=''
             id='user_input'
             placeholder="Batrag in €"
-            onChange={(e) => setValue(Number(e.target.value))}
+            onChange={getUserInput}
             value={value}/>
             <div>
                 <button onClick={deposit} >Einzahlen</button>
